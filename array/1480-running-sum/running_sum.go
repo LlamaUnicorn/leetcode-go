8e7601958f5ad4af91b64fe080f3ae9728a8f2a6
LlamaUnicorn <holidaynight@gmail.com>
2025-01-19 19:12:50 +0300

    init

---
+++ array/1480-running-sum/running_sum.go
@@ -0,0 +1,33 @@
+package runningsum
+
+// RunningSum calculates the running sum of the input array.
+func RunningSum(nums []int) []int {
+	var runSum []int
+	var digit = 0
+	for _, i := range nums {
+		digit += i
+		runSum = append(runSum, digit)
+	}
+	return runSum
+}
+
+//package main
+//
+//import "fmt"
+//
+//var nums = []int{1, 2, 3, 4}
+//
+//func runningSum(nums []int) []int {
+//	var runSum []int
+//	var digit = 0
+//	for _, i := range nums {
+//		fmt.Println(i)
+//		digit += i
+//		runSum = append(runSum, digit)
+//	}
+//	return runSum
+//}
+//
+//func main() {
+//	fmt.Println(runningSum(nums))
+//}
