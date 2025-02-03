2adab57fb7bf43cfe963ab3adc025e1367364540
OzdoevR <ro@webizi.ru>
2025-02-03 14:40:09 +0300

    feat: test 1672

--- array/1672-Richest-Customer-Wealth/main.go
+++ array/1672-Richest-Customer-Wealth/main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+func maximumWealth(accounts [][]int) int {
+	maxWealth := 0
+	for account := range accounts { // [[1,5] [7,3] [3,5]]
+		fmt.Println(accounts[account])
+		accountSum := 0
+		for _, value := range accounts[account] { // [1,5]
+			accountSum += value
+		}
+		fmt.Println(accountSum)
+		fmt.Println(maxWealth)
+		if accountSum > maxWealth {
+			maxWealth = accountSum
+		}
+	}
+	return maxWealth
+}
+
 func main() {
-	fmt.Println("hello")
+	accounts := [][]int{{1, 5}, {7, 3}, {3, 5}}
+	fmt.Println(maximumWealth(accounts))
 }
