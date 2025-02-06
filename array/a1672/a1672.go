28716eb379dddafeb19beb4eda7ffa40ca095cbc
LlamaUnicorn <holidaynight@gmail.com>
2025-02-06 17:34:21 +0300

    feat: 412 fizz
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- array/a1672/a1672.go
+++ array/a1672/a1672.go
@@ -18,3 +18,26 @@ func MaximumWealth(accounts [][]int) int {
 	}
 	return maxWealth
 }
+
+// Accepted, Runtime: 0 ms, Memory Usage: 3.3 MB
+// Runtime: 0 ms, faster than 100.00% of Go online submissions for Richest Customer Wealth.
+func maximumWealth(accounts [][]int) (max int) {
+	calculateWealth := func(account []int, sumCh chan int) {
+		sum := 0
+		for _, val := range account {
+			sum += val
+		}
+		sumCh <- sum
+	}
+
+	for _, account := range accounts {
+		sumCh := make(chan int)
+		go calculateWealth(account, sumCh)
+		accountWealth := <-sumCh
+		if accountWealth > max {
+			max = accountWealth
+		}
+		close(sumCh)
+	}
+	return
+}
---
+++ array/a412/a412.go
@@ -0,0 +1,5 @@
+package a412
+
+func fizzBuzz(n int) []string {
+	return nil
+}
