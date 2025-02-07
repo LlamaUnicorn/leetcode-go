23b8fd1f974c0ee71f0dcbf89f038b4b501721b5
LlamaUnicorn <holidaynight@gmail.com>
2025-02-07 14:03:31 +0300

    feat: 412 fizz
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- array/a412/a412.go
+++ array/a412/a412.go
@@ -1,5 +1,34 @@
 package a412
 
-func fizzBuzz(n int) []string {
-	return nil
+import (
+	"fmt"
+	"strconv"
+)
+
+func FizzBuzz(n int) []string {
+	fmt.Println(n)
+	arr := make([]string, n)
+	for i := 1; i <= n; i++ {
+		arr = append(arr, strconv.Itoa(i))
+	}
+	fmt.Println(arr)
+	//for _, i := range arr {
+	//	switch {
+	//	case i%3 == 0 && i%5 == 0:
+	//		{
+	//			arr[i] = "FizzBuzz"
+	//		}
+	//	case i%3 == 0:
+	//		{
+	//			arr[i] = "Fizz"
+	//		}
+	//	case i%5 == 0:
+	//		{
+	//			arr[i] = "Buzz"
+	//		}
+	//	default:
+	//		arr[i] = strconv.Itoa(i)
+	//	}
+	//}
+	return arr
 }
--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/LlamaUnicorn/leetcode-go/array/a1672"
+	"github.com/LlamaUnicorn/leetcode-go/array/a412"
 )
 
 func main() {
-	accounts := [][]int{{1, 5}, {7, 3}, {3, 5}}
-	fmt.Println(a1672.MaximumWealth(accounts))
-
-	fmt.Println("Hello World")
+	result := a412.FizzBuzz(5)
+	fmt.Println(result)
 }
