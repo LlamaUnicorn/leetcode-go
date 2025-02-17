869bbd6327dbad1731694b0e75a836e9675cbb5a
LlamaUnicorn <holidaynight@gmail.com>
2025-02-17 14:14:04 +0300

    feat: ransom note 383
    moved linked list solution
    
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- linked-list/ll876/ll876.go
+++ linked-list/ll876/ll876.go
@@ -1,11 +1,60 @@
 package ll876
 
-// Definition for singly-linked list.
+import "fmt"
+
+// ListNode Definition for singly-linked list.
 type ListNode struct {
-	Val  int
-	Next *ListNode
+	Value int
+	Next  *ListNode
 }
 
-func MiddleNode(head *ListNode) *ListNode {
+// Function to find the middle node of the linked list
+func middleNode(head *ListNode) *ListNode {
+	// Initialize slow and fast pointers
+	slow, fast := head, head
+
+	// Traverse the list using the two-pointer technique
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next      // Move slow pointer one step
+		fast = fast.Next.Next // Move fast pointer two steps
+	}
+
+	// When fast reaches the end, slow will be at the middle
+	return slow
+}
+
+// Helper function to create a linked list from a slice of integers
+func createLinkedList(values []int) *ListNode {
+	if len(values) == 0 {
+		return nil
+	}
+	head := &ListNode{Value: values[0]}
+	current := head
+	for i := 1; i < len(values); i++ {
+		current.Next = &ListNode{Value: values[i]}
+		current = current.Next
+	}
 	return head
+}
+
+// Helper function to print a linked list
+func printLinkedList(head *ListNode) {
+	current := head
+	for current != nil {
+		fmt.Printf("%d ", current.Value)
+		current = current.Next
+	}
+	fmt.Println()
+}
+
+func main() {
+	// Example 1: [1, 2, 3, 4, 5]
+	head1 := createLinkedList([]int{1, 2, 3, 4, 5})
+	fmt.Print("Middle of [1, 2, 3, 4, 5]: ")
+	printLinkedList(middleNode(head1)) // Output: [3, 4, 5]
+
+	// Example 2: [1, 2, 3, 4, 5, 6]
+	head2 := createLinkedList([]int{1, 2, 3, 4, 5, 6})
+	fmt.Print("Middle of [1, 2, 3, 4, 5, 6]: ")
+	printLinkedList(middleNode(head2)) // Output: [4, 5, 6]
 }
--- string/s383/s383.go
+++ string/s383/s383.go
@@ -1,1 +1,9 @@
 package s383
+
+func RansomNote(note, magazine string) []string {
+	// loop over magazine chars and increment magazine[char] += 1
+	// Loop over note magazine[Note_char] -=1
+	// if negative return false
+
+	return nil
+}
