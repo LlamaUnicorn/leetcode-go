4a7ac9d939788af269ab50da2f1234bca7f57427
LlamaUnicorn <holidaynight@gmail.com>
2025-02-26 13:56:53 +0300

    feat: finish even number of digits
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -5,11 +5,22 @@ import "fmt"
 var nums = []int{12, 345, 2, 6, 7896}
 
 func findNumbers(nums []int) int {
+	total := 0
 	for i := 0; i < len(nums); i++ {
-		nums[i] = nums[i] / 10
-		fmt.Println(nums[i])
+		count := 0
+		for nums[i] > 0 {
+			fmt.Println("num[i]", nums[i])
+			nums[i] /= 10
+			count++
+		}
+		if count%2 == 0 {
+			total += 1
+			fmt.Println("total", total)
+		}
+		fmt.Println("count:", count)
 	}
-	return len(nums)
+	fmt.Println("final total:", total)
+	return total
 }
 func main() {
 	findNumbers(nums)
