f3bd9aab532fb01cca9a83dd19a18db7708f3ddf
LlamaUnicorn <holidaynight@gmail.com>
2025-02-26 06:55:17 +0300

    feat: even number of digits

--- array/even-number-of-digits/end.go
+++ array/even-number-of-digits/end.go
@@ -1,4 +1,4 @@
-package main
+package even_number_of_digits
 
 import "fmt"
 
@@ -10,7 +10,4 @@ func findNumbers(nums []int) int {
 		fmt.Println(nums[i])
 	}
 	return len(nums)
-}
-func main() {
-	findNumbers(nums)
 }
