c98e034cbad2e1bf7398240bed498f8644b8a37a
LlamaUnicorn <holidaynight@gmail.com>
2025-02-27 06:51:38 +0300

    feat: squares of a sorted array

---
+++ array/Squares-of-a-Sorted-Array/sosa.go
@@ -0,0 +1,8 @@
+package Squares_of_a_Sorted_Array
+
+// Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+var nums = []int{-4, -1, 0, 3, 10}
+
+func sortedSquares(nums []int) []int {
+	return nums
+}
--- main.go
+++ main.go
@@ -2,26 +2,22 @@ package main
 
 import "fmt"
 
-var nums = []int{12, 345, 2, 6, 7896}
+var nums = []int{-4, -1, 0, 3, 10}
 
-func findNumbers(nums []int) int {
-	total := 0
-	for i := 0; i < len(nums); i++ {
-		count := 0
-		for nums[i] > 0 {
-			fmt.Println("num[i]", nums[i])
-			nums[i] /= 10
-			count++
+func sortedSquares(nums []int) []int {
+	var result []int
+	for i, j := 0, len(nums); i != j; {
+		if nums[i]*nums[i] >= nums[j]*nums[j] {
+			result = append(result, nums[i])
+			i++
+		} else {
+			result = append(result, nums[j])
+			j--
 		}
-		if count%2 == 0 {
-			total += 1
-			fmt.Println("total", total)
-		}
-		fmt.Println("count:", count)
 	}
-	fmt.Println("final total:", total)
-	return total
+	fmt.Println(result)
+	return result
 }
 func main() {
-	findNumbers(nums)
+	sortedSquares(nums)
 }
