7ac7ea6614f2cc50655bba231cd4cca4eae44598
LlamaUnicorn <holidaynight@gmail.com>
2025-02-28 13:57:34 +0300

    feat: duplicate zeroes
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 var arr = []int{1, 0, 2, 3, 0, 4, 5, 0} // [1,0,0,2,3,0,0,4]
 
 func duplicateZeros(arr []int) {
 	//fmt.Println(arr)
-	for _, elem := range arr {
-		fmt.Println(elem)
+	for idx, elem := range arr {
+		//fmt.Println(elem)
+		if elem == 0 {
+			for i := len(arr) - 1; i > idx; i-- {
+				arr[i+1] = arr[i]
+			}
+		}
 	}
+	fmt.Println(arr)
 }
 
 func main() {
