cda3fe1931831fb5ddb6aae9f74259c24dac10a0
LlamaUnicorn <holidaynight@gmail.com>
2025-03-04 14:13:36 +0300

    feat: merge sorted arrays
    Signed-off-by: OzdoevR <ro@webizi.ru>

---
+++ array/merge_sorted_arrays/msa.go
@@ -0,0 +1,10 @@
+package merge_sorted_arrays
+
+//Input:
+//nums1 = [1,2,3,0,0,0], m = 3,
+//nums2 = [2,5,6], n = 3
+//Output: [1,2,2,3,5,6]
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+
+}
