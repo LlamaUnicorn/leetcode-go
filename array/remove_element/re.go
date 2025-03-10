3537f9c4bac5e0a77df0bf3685ace73422e45655
LlamaUnicorn <holidaynight@gmail.com>
2025-03-10 14:05:31 +0300

    feat: remove element
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- array/remove_element/re.go
+++ array/remove_element/re.go
@@ -1,16 +1,12 @@
-package main
+package remove_element
 
 import "fmt"
 
-var nums = []int{3, 2, 2, 3}
-var val = 3
-
-func main() {
-	removeElement(nums, val)
-}
-
 //Input: nums = [3,2,2,3], val = 3
 //Output: 2, nums = [2,2,_,_]
+
+var nums = []int{3, 2, 2, 3}
+var val = 3
 
 func removeElement(nums []int, val int) int {
 	fmt.Println(nums, val)
