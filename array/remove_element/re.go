a0626a914b28c27d0ba78e36577e776b85efe25e
LlamaUnicorn <holidaynight@gmail.com>
2025-03-13 13:51:40 +0300

    feat: remove element
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- array/remove_element/re.go
+++ array/remove_element/re.go
@@ -3,7 +3,10 @@ package remove_element
 //Input: nums = [3,2,2,3], val = 3
 //Output: 2, nums = [2,2,_,_]
 
-var nums = []int{3, 2, 2, 3}
+//var nums = []int{3, 2, 2, 3}
+//var val = 3
+
+var nums = []int{2}
 var val = 3
 
 func removeElement(nums []int, val int) int {
