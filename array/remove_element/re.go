d0ddbb4be9365c660431457053ac677efb3d9883
LlamaUnicorn <holidaynight@gmail.com>
2025-03-17 06:54:09 +0300

    feat: remove element

--- array/remove_element/re.go
+++ array/remove_element/re.go
@@ -1,24 +1,24 @@
 package remove_element
 
+import "fmt"
+
 //Input: nums = [3,2,2,3], val = 3
 //Output: 2, nums = [2,2,_,_]
 
-//var nums = []int{3, 2, 2, 3}
-//var val = 3
-
-var nums = []int{2}
+var nums = []int{3, 2, 2, 3}
 var val = 3
+
+//var nums = []int{2} //[2]
+//var val = 3
 
 func removeElement(nums []int, val int) int {
 	ptr := 0
-	for i := 0; i < len(nums)-1; i++ {
-		if nums[i] == val {
-			continue
-		} else {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
 			nums[ptr] = nums[i]
 			ptr++
 		}
 	}
-	nums = nums[0 : len(nums)-ptr]
+	fmt.Println(nums)
 	return ptr
 }
--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
-//var nums = []int{3, 2, 2, 3}
-//var val = 3
+import "fmt"
 
-var nums = []int{2} //[2]
+var nums = []int{3, 2, 2, 3}
 var val = 3
+
+//Input: nums = [3,2,2,3], val = 3
+//Output: 2, nums = [2,2,_,_]
+
+//var nums = []int{2} //[2]
+//var val = 3
 
 func main() {
 	removeElement(nums, val)
 }
 
-//Input: nums = [3,2,2,3], val = 3
-//Output: 2, nums = [2,2,_,_]
-
 func removeElement(nums []int, val int) int {
 	ptr := 0
-	for i := 0; i < len(nums)-1; i++ {
-		if nums[i] == val {
-			continue
-		} else {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
 			nums[ptr] = nums[i]
 			ptr++
 		}
 	}
-	nums = nums[0 : len(nums)-ptr]
+	//nums = nums[0 : len(nums)-ptr]
+	fmt.Println(nums)
 	return ptr
 }
