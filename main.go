6b5b0adce762b27d2691edc4c941361e63493ea0
LlamaUnicorn <holidaynight@gmail.com>
2025-03-19 06:54:27 +0300

    feat: remove duplicate elements from sorted array

--- main.go
+++ main.go
@@ -4,19 +4,41 @@ import "fmt"
 
 // Input: nums = [1,1,2]
 // Output: 2, nums = [1,2,_]
-var nums = []int{1, 1, 2}
+//var nums = []int{1, 1, 2}
 
 //Input: nums = [0,0,1,1,1,2,2,3,3,4]
 //Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 //nums := []int{0,0,1,1,1,2,2,3,3,4}
 
+//Input: [0,0,1,1,1,2,2,3,3,4]
+//Output: [0,1,1,2,1,3,2,4]
+//Expected: [0,1,2,3,4]
+
+var nums = []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+
 func removeDuplicates(nums []int) int {
 	ptr := 0
-	uniquePtr := 0
+	uniqueCtr := 0
 	for i := 0; i < len(nums); i++ {
-		fmt.Println(i, ptr, uniquePtr)
+		if i == 0 {
+			continue
+		}
+		if i != 0 && nums[i-1] == nums[i] {
+			uniqueCtr++
+			if uniqueCtr == 1 {
+				ptr++
+			}
+			continue
+		}
+
+		nums[ptr] = nums[i]
+		ptr++
+		uniqueCtr = 0
+
+		fmt.Println(i, ptr, nums[i])
 	}
-	return 0
+	fmt.Println(nums)
+	return ptr
 }
 
 func main() {
