a8337cc5facdb46caee029b593af6e512eda030c
LlamaUnicorn <holidaynight@gmail.com>
2025-03-20 17:43:19 +0300

    feat: valid mountain array
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -2,41 +2,23 @@ package main
 
 import "fmt"
 
-//Given an array arr of integers, check if there exist two indices i and j such that :
-
-// i != j
-// 0 <= i, j < arr.length
-// arr[i] == 2 * arr[j]
-
-// Input: arr = [10,2,5,3]
-// Output: true
-// Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
-//var arr = []int{10, 2, 5, 3}
-
-//Input: arr = [3,1,7,11]
-//Output: false
-//Explanation: There is no i and j that satisfy the conditions.
-//var arr = []int{3,1,7,11}
-
 // Input:
-// [7,1,14,11]
-// Output:
-// false
-// Expected:
-// true
-var arr = []int{7, 1, 14, 11}
+var arr = []int{2, 1}
 
-func checkIfExist(arr []int) bool {
-	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(arr); j++ {
-			if i != j && arr[i] == 2*arr[j] {
-				return true
-			}
-		}
-	}
-	return false
+//Output: false
+//Example 2:
+//
+//Input: arr = [3,5,5]
+//Output: false
+//Example 3:
+//
+//Input: arr = [0,3,2,1]
+//Output: true
+
+func validMountainArray(arr []int) bool {
+	return true
 }
 
 func main() {
-	fmt.Println(checkIfExist(arr))
+	fmt.Println(validMountainArray(arr))
 }
