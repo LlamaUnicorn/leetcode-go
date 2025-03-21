6577faee9fe6b3f1ad850d316ff9d41b9b20ff83
LlamaUnicorn <holidaynight@gmail.com>
2025-03-21 07:04:59 +0300

    feat: valid mountain array

--- array/valid_mountain_array/vma.go
+++ array/valid_mountain_array/vma.go
@@ -30,6 +30,52 @@ package valid_mountain_array
 //Hide Hint #1
 //It's very easy to keep track of a monotonically increasing or decreasing ordering of elements. You just need to be able to determine the start of the valley in the mountain and from that point onwards, it should be a valley i.e. no mini-hills after that. Use this information in regards to the values in the array and you will be able to come up with a straightforward solution.
 
+// Input:
+var arr = []int{0, 3, 2, 1}
+
+//Output: false
+
+//var arr = []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+
 func validMountainArray(arr []int) bool {
-	return true
+	ptr := 0
+	for i := 1; i <= len(arr)-1; i++ {
+		if (arr[i-1] < arr[i]) && (i-ptr == 1) {
+			ptr++
+			continue
+		} else if arr[i-1] > arr[i] {
+			if i == len(arr)-1 && ptr != 0 {
+				return true
+			}
+			continue
+		}
+		return false
+	}
+	return false
 }
+
+//func validMountainArray(arr []int) bool {
+//	n := len(arr)
+//	if n < 3 {
+//		return false
+//	}
+//
+//	peak := 0
+//	// Find the peak index where the increasing phase ends
+//	for peak < n-1 && arr[peak] < arr[peak+1] {
+//		peak++
+//	}
+//
+//	// Peak cannot be the first or last element
+//	if peak == 0 || peak == n-1 {
+//		return false
+//	}
+//
+//	// Check the decreasing phase from the peak
+//	for peak < n-1 && arr[peak] > arr[peak+1] {
+//		peak++
+//	}
+//
+//	// If we reached the end of the array, it's a valid mountain
+//	return peak == n-1
+//}
--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 // Input:
-var arr = []int{2, 1}
+var arr = []int{0, 3, 2, 1}
 
 //Output: false
+
 //Example 2:
 //
 //Input: arr = [3,5,5]
@@ -15,8 +16,23 @@ var arr = []int{2, 1}
 //Input: arr = [0,3,2,1]
 //Output: true
 
+//var arr = []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+
 func validMountainArray(arr []int) bool {
-	return true
+	ptr := 0
+	for i := 1; i <= len(arr)-1; i++ {
+		if (arr[i-1] < arr[i]) && (i-ptr == 1) {
+			ptr++
+			continue
+		} else if arr[i-1] > arr[i] {
+			if i == len(arr)-1 && ptr != 0 {
+				return true
+			}
+			continue
+		}
+		return false
+	}
+	return false
 }
 
 func main() {
