6e254d346ad0544db4d22708e24abed544504694
LlamaUnicorn <holidaynight@gmail.com>
2025-03-26 06:58:12 +0300

    feat: sort array by parity

--- main.go
+++ main.go
@@ -6,6 +6,20 @@ var nums = []int{3, 1, 2, 4} // [2,4,3,1]
 //var nums = []int{0}  // [0]
 
 func sortArrayByParity(nums []int) []int {
+	left, right := 0, 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i]%2 != 0 {
+			for j := right; j > i; j-- {
+				if nums[j]%2 == 0 {
+					temp := nums[i]
+					nums[i] = nums[j]
+					nums[j] = temp
+					right--
+					left++
+				}
+			}
+		}
+	}
 	return nums
 }
 
