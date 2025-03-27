34f8d0dc92b5b9336831e7fd7df546fef4b2cb40
LlamaUnicorn <holidaynight@gmail.com>
2025-03-27 13:49:32 +0300

    feat: height checker failed
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import "fmt"
 
-var heights = []int{1, 1, 4, 2, 1, 3} // 3
-//var heights = []int{5, 1, 2, 3, 4} // 5
+// var heights = []int{1, 1, 4, 2, 1, 3} // 3
+var heights = []int{5, 1, 2, 3, 4} // 5  index out of range
 //var heights = []int{1,2,3,4,5} // 0
 
 func heightChecker(heights []int) int {
-	res := []int{}
+	res := make([]int, len(heights))
 	for i := 0; i < len(heights); i++ {
-		if i == 0 || heights[i] >= heights[i-1] {
+		//fmt.Println(res)
+		if i == 0 || heights[i] >= res[i-1] {
 			res[i] = heights[i]
 		} else {
 			for j := i; heights[i] < res[j-1]; j-- {
@@ -17,9 +18,11 @@ func heightChecker(heights []int) int {
 			}
 		}
 	}
+	fmt.Println(res)
 	ctr := 0
 	for i := 0; i < len(heights); i++ {
 		if heights[i] != res[i] {
+			//fmt.Println(heights[i], res[i])
 			ctr++
 		}
 	}
