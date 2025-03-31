cfb13dae94fc5ad0c2cdba2fe4c6805a329894a3
LlamaUnicorn <holidaynight@gmail.com>
2025-03-31 14:13:08 +0300

    feat: third distinct maximum
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -2,36 +2,14 @@ package main
 
 import "fmt"
 
-// var heights = []int{1, 1, 4, 2, 1, 3} // 3
-// var heights = []int{5, 1, 2, 3, 4} // 5
-var heights = []int{1, 2, 3, 4, 5} // 0
+var nums = []int{3, 2, 1} // 1
+//var nums = []int{1, 2} // 2
+//var nums = []int{2, 2, 3, 1} // 1
 
-func heightChecker(heights []int) int {
-	res := make([]int, len(heights))
-	for i := 0; i < len(heights); i++ {
-		//fmt.Println(res)
-		if i == 0 || heights[i] >= res[i-1] {
-			res[i] = heights[i]
-		} else {
-			for j := i; heights[i] < res[j-1]; j-- {
-				res[j-1], res[j] = heights[i], res[j-1]
-				if j-1 == 0 {
-					break
-				}
-			}
-		}
-	}
-	fmt.Println(res)
-	ctr := 0
-	for i := 0; i < len(heights); i++ {
-		if heights[i] != res[i] {
-			//fmt.Println(heights[i], res[i])
-			ctr++
-		}
-	}
-	return ctr
+func thirdMax(nums []int) int {
+	return 0
 }
 
 func main() {
-	fmt.Println(heightChecker(heights))
+	fmt.Println(thirdMax(nums))
 }
