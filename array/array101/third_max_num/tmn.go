bd971a5d06bf096938b005e99db3009bdc53b9a9
LlamaUnicorn <holidaynight@gmail.com>
2025-04-02 06:55:42 +0300

    feat: third distinct maximum

--- array/array101/third_max_num/tmn.go
+++ array/array101/third_max_num/tmn.go
@@ -40,6 +40,36 @@ package third_max_num
 // var nums = []int{3,2,1} // 1
 var nums = []int{1, 2} // 2
 
+//fmt.Println(thirdMax([]int{3, 2, 1}))    // Output: 1
+//fmt.Println(thirdMax([]int{1, 2}))       // Output: 2
+//fmt.Println(thirdMax([]int{2, 2, 3, 1})) // Output: 1
+//fmt.Println(thirdMax([]int{5}))          // Output: 5
+//fmt.Println(thirdMax([]int{2, 2, 2}))    // Output: 2
+//fmt.Println(thirdMax([]int{-1, -2, -3})) // Output: -3
+
 func thirdMax(nums []int) int {
-	return 0
+	var max1, max2, max3 *int
+	for i := range nums {
+		num := nums[i]
+
+		if (max1 != nil && num == *max1) || (max2 != nil && num == *max2) || (max3 != nil && num == *max3) {
+			continue
+		}
+
+		if max1 == nil || num > *max1 {
+			max3 = max2
+			max2 = max1
+			max1 = &nums[i]
+		} else if max2 == nil || num > *max2 {
+			max3 = max2
+			max2 = &nums[i]
+		} else if max3 == nil || num > *max3 {
+			max3 = &nums[i]
+		}
+	}
+
+	if max3 != nil {
+		return *max3
+	}
+	return *max1
 }
