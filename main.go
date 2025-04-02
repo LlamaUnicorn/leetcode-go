3a4c8887f097354a8b7e06ff2c2ce266cea66299
LlamaUnicorn <holidaynight@gmail.com>
2025-04-02 06:53:46 +0300

    feat: third distinct maximum

--- main.go
+++ main.go
@@ -1,36 +1,41 @@
 package main
 
-import "fmt"
-
-// var nums = []int{3, 2, 1} // 1
-var nums = []int{1, 2} // 2
-//var nums = []int{2, 2, 3, 1} // 1
+import (
+	"fmt"
+)
 
 func thirdMax(nums []int) int {
 	var max1, max2, max3 *int
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == *max1 || nums[i] == *max2 || nums[i] == *max3 {
+	for i := range nums {
+		num := nums[i]
+
+		if (max1 != nil && num == *max1) || (max2 != nil && num == *max2) || (max3 != nil && num == *max3) {
 			continue
 		}
-		if max1 == nil || nums[i] > *max1 {
-			max2 = max1
+
+		if max1 == nil || num > *max1 {
 			max3 = max2
+			max2 = max1
 			max1 = &nums[i]
-		} else if max2 == nil || nums[i] > *max2 {
+		} else if max2 == nil || num > *max2 {
 			max3 = max2
 			max2 = &nums[i]
-		} else if max3 == nil || nums[i] > *max3 {
+		} else if max3 == nil || num > *max3 {
 			max3 = &nums[i]
 		}
 	}
-	fmt.Println(*max1, *max2, *max3)
+
 	if max3 != nil {
 		return *max3
-	} else {
-		return *max1
 	}
+	return *max1
 }
 
 func main() {
-	fmt.Println(thirdMax(nums))
+	fmt.Println(thirdMax([]int{3, 2, 1}))    // Output: 1
+	fmt.Println(thirdMax([]int{1, 2}))       // Output: 2
+	fmt.Println(thirdMax([]int{2, 2, 3, 1})) // Output: 1
+	fmt.Println(thirdMax([]int{5}))          // Output: 5
+	fmt.Println(thirdMax([]int{2, 2, 2}))    // Output: 2
+	fmt.Println(thirdMax([]int{-1, -2, -3})) // Output: -3
 }
