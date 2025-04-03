1cd9d5a511dde3b1035ed06c385adc6835870607
LlamaUnicorn <holidaynight@gmail.com>
2025-04-03 14:26:05 +0300

    feat: third distinct maximum
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+var nums = []int{4, 3, 2, 7, 8, 2, 3, 1} //Output: [5,6]
+//var nums = []int{1,1} //Output: [2]
+
 func findDisappearedNumbers(nums []int) []int {
 	return []int{0}
 }
