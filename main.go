02370a37dd14facd3a02dcf0c98922fd150cbfc4
LlamaUnicorn <holidaynight@gmail.com>
2025-04-04 14:34:34 +0300

    feat: third distinct maximum
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -8,23 +8,25 @@ var nums = []int{4, 3, 2, 7, 8, 2, 3, 1} //Output: [5,6]
 //var nums = []int{1,1} //Output: [2]
 
 func findDisappearedNumbers(nums []int) []int {
-	notFound := false
 	pointer := 0
-	for i := 1; i < len(nums); i++ {
+	for i := 1; i <= len(nums); i++ {
+		found := false
 		for j := 0; j < len(nums); j++ {
 			if nums[j] == i {
+				found = true
 				break
 			}
 		}
-		if notFound {
-			nums[i] = i
+		if !found {
+			nums[pointer] = i
 			pointer++
 		}
 	}
-	return nums
+	return nums[:pointer]
 }
 
 func main() {
-	fmt.Println(findDisappearedNumbers([]int{4, 3, 2, 7, 8, 2, 3, 1})) // Output: [5,6]
-	fmt.Println(findDisappearedNumbers([]int{1, 1}))                   // Output: [2]
+	fmt.Println(findDisappearedNumbers([]int{4, 3, 2, 7, 8, 2, 3, 1}))        // Output: [5,6]
+	fmt.Println(findDisappearedNumbers([]int{1, 1}))                          // Output: [2]
+	fmt.Println(findDisappearedNumbers([]int{5, 4, 6, 7, 9, 3, 10, 9, 5, 6})) // Output: [1, 2, 8]
 }
