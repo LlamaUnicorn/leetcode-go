f719a9c707419af543fedeb40a771de5a94f5264
LlamaUnicorn <holidaynight@gmail.com>
2025-04-04 06:59:04 +0300

    feat: numbers disappeared in an array

--- main.go
+++ main.go
@@ -8,7 +8,20 @@ var nums = []int{4, 3, 2, 7, 8, 2, 3, 1} //Output: [5,6]
 //var nums = []int{1,1} //Output: [2]
 
 func findDisappearedNumbers(nums []int) []int {
-	return []int{0}
+	notFound := false
+	pointer := 0
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < len(nums); j++ {
+			if nums[j] == i {
+				break
+			}
+		}
+		if notFound {
+			nums[i] = i
+			pointer++
+		}
+	}
+	return nums
 }
 
 func main() {
