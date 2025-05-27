1ad871ae000a3d052a4193a3f6faba4f7f1d8b3d
LlamaUnicorn <holidaynight@gmail.com>
2025-05-27 14:06:54 +0300

    feat: Largest Number At Least Twice of Others
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -2,29 +2,11 @@ package main
 
 import "fmt"
 
-func pivotIndex(nums []int) int {
-	leftSum := 0
-	rightSum := 0
-	for i := 0; i < len(nums); i++ {
-		leftSum = 0
-		rightSum = 0
-		for j := 0; j < len(nums); j++ {
-			if j < i {
-				leftSum += nums[j]
-			}
-			if j > i {
-				rightSum += nums[j]
-			}
-		}
-		if leftSum == rightSum {
-			return i
-		}
-	}
+func dominantIndex(nums []int) int {
 	return -1
 }
 
 func main() {
-	//fmt.Println(pivotIndex([]int{1, 7, 3, 6, 5, 6})) // Output: 3
-	//fmt.Println(pivotIndex([]int{1, 2, 3})) // Output: -1
-	fmt.Println(pivotIndex([]int{2, 1, -1})) // Output: 0
+	fmt.Println(dominantIndex([]int{3, 6, 1, 0})) // Output: 1
+	fmt.Println(dominantIndex([]int{1, 2, 3, 4})) // Output: -1
 }
