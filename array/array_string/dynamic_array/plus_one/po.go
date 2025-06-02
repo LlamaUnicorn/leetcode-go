6ddbf1b54afb57fff2be315d032e551b58738d2a
LlamaUnicorn <holidaynight@gmail.com>
2025-06-02 13:35:02 +0300

    feat: finish Plus one
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- array/array_string/dynamic_array/plus_one/po.go
+++ array/array_string/dynamic_array/plus_one/po.go
@@ -1,5 +1,9 @@
 package plus_one
 
+import (
+	"slices"
+)
+
 //You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 //
 //Increment the large integer by one and return the resulting array of digits.
@@ -34,5 +38,25 @@ package plus_one
 //digits does not contain any leading 0's.
 
 func plusOne(digits []int) []int {
-	return []int{1}
+	adder := 1
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i]+1 > 9 {
+			adder := (digits[i] + adder) / 10
+			digits[i] = (digits[i] + adder) % 10
+		} else {
+			digits[i] += 1
+			return digits
+		}
+		if i == 0 {
+			digits = slices.Insert(digits, 0, adder)
+			return digits
+		}
+	}
+	return digits
 }
+
+//func main() {
+//	fmt.Println(plusOne([]int{1, 2, 3}))    // Output: [1,2,4]
+//	fmt.Println(plusOne([]int{4, 3, 2, 1})) // Output: [4,3,2,2]
+//	fmt.Println(plusOne([]int{1, 9})) // Output: [1,0]
+//}
--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func plusOne(digits []int) []int {
-	return []int{1}
+	adder := 1
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i]+1 > 9 {
+			adder := (digits[i] + adder) / 10
+			digits[i] = (digits[i] + adder) % 10
+		} else {
+			digits[i] += 1
+			return digits
+		}
+		if i == 0 {
+			digits = slices.Insert(digits, 0, adder)
+			return digits
+		}
+	}
+	return digits
 }
 
 func main() {
 	fmt.Println(plusOne([]int{1, 2, 3}))    // Output: [1,2,4]
 	fmt.Println(plusOne([]int{4, 3, 2, 1})) // Output: [4,3,2,2]
-	fmt.Println(plusOne([]int{9}))          // Output: [1,0]
+	fmt.Println(plusOne([]int{1, 9}))       // Output: [1,0]
 }
