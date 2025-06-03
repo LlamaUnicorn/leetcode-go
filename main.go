89cbe9fe4ea79d2e86e87ee04febfd5b55a12635
LlamaUnicorn <holidaynight@gmail.com>
2025-06-03 13:36:28 +0300

    feat: diagonal traverse
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -2,29 +2,13 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
 
-func plusOne(digits []int) []int {
-	adder := 1
-	for i := len(digits) - 1; i >= 0; i-- {
-		if digits[i]+1 > 9 {
-			adder := (digits[i] + adder) / 10
-			digits[i] = (digits[i] + adder) % 10
-		} else {
-			digits[i] += 1
-			return digits
-		}
-		if i == 0 {
-			digits = slices.Insert(digits, 0, adder)
-			return digits
-		}
-	}
-	return digits
+func findDiagonalOrder(mat [][]int) []int {
+	return []int{1}
 }
 
 func main() {
-	fmt.Println(plusOne([]int{1, 2, 3}))    // Output: [1,2,4]
-	fmt.Println(plusOne([]int{4, 3, 2, 1})) // Output: [4,3,2,2]
-	fmt.Println(plusOne([]int{1, 9}))       // Output: [1,0]
+	fmt.Println(findDiagonalOrder([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})) // Output: [1,2,4,7,5,3,6,8,9]
+	fmt.Println(findDiagonalOrder([][]int{{1, 2}, {3, 4}}))                  // Output: [1,2,3,4// ]
 }
