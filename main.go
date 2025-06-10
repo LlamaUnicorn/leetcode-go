6296e166e2ec20bd09c03436514c173c786d71b7
LlamaUnicorn <holidaynight@gmail.com>
2025-06-10 14:16:41 +0300

    feat: diagonal traverse didn't get the solution
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -5,7 +5,44 @@ import (
 )
 
 func findDiagonalOrder(mat [][]int) []int {
-	return []int{1}
+	m, n := len(mat), len(mat[0])
+	result := make([]int, m*n)
+	idx := 0
+
+	// Process each diagonal (identified by sum i+j)
+	for diag := 0; diag < m+n-1; diag++ {
+		if diag%2 == 0 {
+			// Even diagonal: traverse up-right (decreasing row, increasing col)
+			for i := min(diag, m-1); i >= 0 && diag-i < n; i-- {
+				j := diag - i
+				result[idx] = mat[i][j]
+				idx++
+			}
+		} else {
+			// Odd diagonal: traverse down-left (increasing row, decreasing col)
+			for i := max(0, diag-n+1); i <= diag && i < m; i++ {
+				j := diag - i
+				result[idx] = mat[i][j]
+				idx++
+			}
+		}
+	}
+
+	return result
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func main() {
