1ddb60342d0e361af20923ddc63a0c5699c4fc13
LlamaUnicorn <holidaynight@gmail.com>
2025-06-16 13:57:35 +0300

    feat: add binary
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -11,10 +11,14 @@ import (
 // iterate over the next, keeping the sum
 
 func addBinary(a string, b string) string {
-	return a
+	for i := len(a) - 1; i >= 0; i-- {
+		fmt.Printf("a[%d] %c\n", i, a[i])
+		//fmt.Print(b[i])
+	}
+	return a + b
 }
 
 func main() {
-	fmt.Println(addBinary("11", "1"))      // Output: 100
-	fmt.Println(addBinary("1010", "1011")) // Output: 10101
+	fmt.Println(addBinary("11", "1")) // Output: 100
+	//fmt.Println(addBinary("1010", "1011")) // Output: 10101
 }
