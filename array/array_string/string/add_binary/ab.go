40cfbdd58a20cd2fed3d2ff181fc892c5abd47af
LlamaUnicorn <holidaynight@gmail.com>
2025-06-17 14:05:33 +0300

    feat: add binary
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- array/array_string/string/add_binary/ab.go
+++ array/array_string/string/add_binary/ab.go
@@ -18,6 +18,46 @@ package add_binary
 //a and b consist only of '0' or '1' characters.
 //Each string does not contain leading zeros except for the zero itself.
 
-func addBinary(a string, b string) string {
-	return a
-}
+//import (
+//	"fmt"
+//	"strings"
+//)
+//
+//func addBinary(a string, b string) string {
+//	var result strings.Builder
+//	carry := 0
+//	i, j := len(a)-1, len(b)-1
+//
+//	// Process from right to left
+//	for i >= 0 || j >= 0 || carry > 0 {
+//		sum := carry
+//
+//		if i >= 0 {
+//			sum += int(a[i] - '0')
+//			i--
+//		}
+//
+//		if j >= 0 {
+//			sum += int(b[j] - '0')
+//			j--
+//		}
+//
+//		result.WriteByte(byte(sum%2) + '0')
+//		carry = sum / 2
+//	}
+//
+//	// Reverse the result since we built it backwards
+//	s := result.String()
+//	runes := []rune(s)
+//	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+//		runes[i], runes[j] = runes[j], runes[i]
+//	}
+//
+//	return string(runes)
+//}
+//
+//func main() {
+//	// Test cases
+//	fmt.Println(addBinary("11", "1"))      // "100"
+//	fmt.Println(addBinary("1010", "1011")) // "10101"
+//}
