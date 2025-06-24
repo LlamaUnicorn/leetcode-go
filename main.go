72c2527bab793751ba67e412e86bd5091e3b0b38
LlamaUnicorn <holidaynight@gmail.com>
2025-06-24 14:05:46 +0300

    feat: longest common prefix
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -4,28 +4,13 @@ import (
 	"fmt"
 )
 
-func strStr(haystack string, needle string) int {
-	n := len(needle)
-	if n == 0 {
-		return 0
-	}
-
-	h := len(haystack)
-	if h < n {
-		return -1
-	}
-
-	for i := 0; i <= h-n; i++ {
-		if haystack[i:i+n] == needle {
-			return i
-		}
-	}
-
-	return -1
+func longestCommonPrefix(strs []string) string {
+	//longest := ""
+	return ""
 }
 
 func main() {
 	// Test cases
-	fmt.Println(strStr("sadbutsad", "sad"))  // 0
-	fmt.Println(strStr("leetcode", "leeto")) // -1
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"})) // "fl"
+	fmt.Println(longestCommonPrefix([]string{"dog", "racecar", "car"}))    // ""
 }
