87981bec49189e78779685beafe00639c36c3863
LlamaUnicorn <holidaynight@gmail.com>
2025-06-25 14:24:24 +0300

    feat: longest common prefix
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -6,6 +6,21 @@ import (
 
 func longestCommonPrefix(strs []string) string {
 	//longest := ""
+	//for _, word := range strs {
+	//	fmt.Println(word)
+	//	for i := 0; i < len(word); i++ {
+	//		fmt.Println(string(word[i]))
+	//	}
+	//}
+	for _, letter := range strs[0] {
+		fmt.Println(string(letter))
+		for i := 1; i < len(strs); i++ {
+			fmt.Println(string(strs[i]))
+			for j := 0; j < len(strs[i]); j++ {
+				fmt.Println(string(strs[i][j]))
+			}
+		}
+	}
 	return ""
 }
 
