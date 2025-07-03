b4b4c653015f482e7a47bb01a170cfa43ecabd1d
OzdoevR <ro@webizi.ru>
2025-07-03 14:25:41 +0300

    feat: min size subarray sum
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- main.go
+++ main.go
@@ -4,9 +4,56 @@ import (
 	"fmt"
 )
 
+//func minSubArrayLen(target int, nums []int) int {
+//	return 0
+//}
+
+// Sliding Window (O(n))
 func minSubArrayLen(target int, nums []int) int {
-	return 0
+	left, sum, minLen := 0, 0, len(nums)+1
+	for right := 0; right < len(nums); right++ {
+		sum += nums[right]
+		for sum >= target {
+			currLen := right - left + 1
+			if currLen < minLen {
+				minLen = currLen
+			}
+			sum -= nums[left]
+			left++
+		}
+	}
+	if minLen == len(nums)+1 {
+		return 0
+	}
+	return minLen
 }
+
+//Prefix + Binary Search (O(n log n))
+//import "sort"
+//
+//func minSubArrayLenBS(target int, nums []int) int {
+//	n := len(nums)
+//	P := make([]int, n+1)
+//	for i := 1; i <= n; i++ {
+//		P[i] = P[i-1] + nums[i-1]
+//	}
+//	minLen := n + 1
+//	for i := 0; i < n; i++ {
+//		needed := target + P[i]
+//		j := sort.Search(n+1, func(j int) bool {
+//			return P[j] >= needed
+//		})
+//		if j <= n {
+//			if j-i < minLen {
+//				minLen = j - i
+//			}
+//		}
+//	}
+//	if minLen == n+1 {
+//		return 0
+//	}
+//	return minLen
+//}
 
 func main() {
 	// Test cases
