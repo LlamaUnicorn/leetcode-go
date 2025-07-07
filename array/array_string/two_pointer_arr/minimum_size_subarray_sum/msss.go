fb4af97856094f0e8b2a62d270fa21b0c8afdd60
LlamaUnicorn <holidaynight@gmail.com>
2025-07-07 13:45:22 +0300

    feat: rotate array
    Signed-off-by: OzdoevR <ro@webizi.ru>

--- array/array_string/two_pointer_arr/minimum_size_subarray_sum/msss.go
+++ array/array_string/two_pointer_arr/minimum_size_subarray_sum/msss.go
@@ -31,5 +31,54 @@ package minimum_size_subarray_sum
 //Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
 
 func minSubArrayLen(target int, nums []int) int {
-	return 0
+	left, sum, minLen := 0, 0, len(nums)+1
+	for right := 0; right < len(nums); right++ {
+		sum += nums[right]
+		for sum >= target {
+			currLen := right - left + 1
+			if currLen < minLen {
+				minLen = currLen
+			}
+			sum -= nums[left]
+			left++
+		}
+	}
+	if minLen == len(nums)+1 {
+		return 0
+	}
+	return minLen
 }
+
+//Prefix + Binary Search (O(n log n))
+//import "sort"
+//
+//func minSubArrayLenBS(target int, nums []int) int {
+//	n := len(nums)
+//	P := make([]int, n+1)
+//	for i := 1; i <= n; i++ {
+//		P[i] = P[i-1] + nums[i-1]
+//	}
+//	minLen := n + 1
+//	for i := 0; i < n; i++ {
+//		needed := target + P[i]
+//		j := sort.Search(n+1, func(j int) bool {
+//			return P[j] >= needed
+//		})
+//		if j <= n {
+//			if j-i < minLen {
+//				minLen = j - i
+//			}
+//		}
+//	}
+//	if minLen == n+1 {
+//		return 0
+//	}
+//	return minLen
+//}123
+
+//func main() {
+//	// Test cases
+//	fmt.Println(minSubArrayLen(7, []int{2, 3, 1, 2, 4, 3}))        // 2
+//	fmt.Println(minSubArrayLen(4, []int{1, 4, 4}))                 //
+//	fmt.Println(minSubArrayLen(11, []int{1, 1, 1, 1, 1, 1, 1, 1})) // 0
+//}
--- main.go
+++ main.go
@@ -4,60 +4,15 @@ import (
 	"fmt"
 )
 
-//func minSubArrayLen(target int, nums []int) int {
-//	return 0
-//}
+func rotate(nums []int, k int) {
 
-// Sliding Window (O(n))
-func minSubArrayLen(target int, nums []int) int {
-	left, sum, minLen := 0, 0, len(nums)+1
-	for right := 0; right < len(nums); right++ {
-		sum += nums[right]
-		for sum >= target {
-			currLen := right - left + 1
-			if currLen < minLen {
-				minLen = currLen
-			}
-			sum -= nums[left]
-			left++
-		}
-	}
-	if minLen == len(nums)+1 {
-		return 0
-	}
-	return minLen
 }
-
-//Prefix + Binary Search (O(n log n))
-//import "sort"
-//
-//func minSubArrayLenBS(target int, nums []int) int {
-//	n := len(nums)
-//	P := make([]int, n+1)
-//	for i := 1; i <= n; i++ {
-//		P[i] = P[i-1] + nums[i-1]
-//	}
-//	minLen := n + 1
-//	for i := 0; i < n; i++ {
-//		needed := target + P[i]
-//		j := sort.Search(n+1, func(j int) bool {
-//			return P[j] >= needed
-//		})
-//		if j <= n {
-//			if j-i < minLen {
-//				minLen = j - i
-//			}
-//		}
-//	}
-//	if minLen == n+1 {
-//		return 0
-//	}
-//	return minLen
-//}123
 
 func main() {
 	// Test cases
-	fmt.Println(minSubArrayLen(7, []int{2, 3, 1, 2, 4, 3}))        // 2
-	fmt.Println(minSubArrayLen(4, []int{1, 4, 4}))                 //
-	fmt.Println(minSubArrayLen(11, []int{1, 1, 1, 1, 1, 1, 1, 1})) // 0
+	arr := []int{1, 2, 3, 4, 5, 6, 7}
+	rotate([]int{1, 2, 3, 4, 5, 6, 7}, 3)
+	//rotate([]int{-1, -100, 3, 99}, 2)
+	fmt.Println(arr) // [5,6,7,1,2,3,4]
+	//fmt.Println(arr) // [3,99,-1,-100]
 }
